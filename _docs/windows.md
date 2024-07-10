da43a00ce8477df30b1ac8e38562e03f59deba97
Pontus Henriksson <pontushenrikssn@gmail.com>
2024-07-10 05:50:09 +0200

    updated info

--- _docs/windows.md
+++ _docs/windows.md
@@ -8,3 +8,5 @@ modified at: 2024-07-10
 ```
 
 ## heading
+
+- Screaming Frog SEO Spider
---
+++ windows/setup-winget.sh
@@ -0,0 +1,72 @@
+# Package managers
+winget install -e --id Yarn.Yarn
+winget install -e --id pnpm.pnpm
+
+# File management
+winget install -e --id vim.vim
+winget install -e --id Neovim.Neovim
+
+# Studies
+winget install -e --id Anki.Anki
+winget install -e --id Obsidian.Obsidian
+
+# Communication
+winget install -e --id Zoom.Zoom
+winget install -e --id OpenWhisperSystems.Signal
+winget install -e --id Discord.Discord
+winget install -e --id ProtonTechnologies.ProtonMailBridge
+winget install -e --id Grammarly.Grammarly
+winget install -e --id SlackTechnologies.Slack
+
+# Media
+winget install -e --id VideoLAN.VLC
+winget install -e --id Spotify.Spotify
+winget install -e --id Audacity.Audacity
+winget install -e --id OBSProject.OBSStudio
+winget install -e --id qBittorrent.qBittorrent
+
+# Game libraries
+winget install -e --id Valve.Steam
+winget install -e --id EpicGames.EpicGamesLauncher
+winget install -e --id Ubisoft.Connect
+
+# Game engines
+winget install -e --id Unity.UnityHub
+
+# Reverse engineering
+winget install -e --id WiresharkFoundation.Wireshark
+winget install -e --id Postman.Postman
+winget install -e --id ApacheFriends.Xampp.8.2
+
+# Clear shit
+winget install -e --id Klocman.BulkCrapUninstaller
+
+# Web browsers
+winget install -e --id Mozilla.Firefox.Nightly
+
+# Essential
+winget install -e --id Bitwarden.Bitwarden
+winget install -e --id Git.Git
+winget install -e --id OpenJS.NodeJS
+winget install -e --id Piriform.Speccy
+winget install -e --id Python.Python.3.11
+winget install -e --id Synology.DriveClient
+
+# ISO Burners
+winget install -e --id Rufus.Rufus
+winget install -e --id Balena.Etcher
+
+# Designing
+winget install -e --id Figma.Figma
+winget install -e --id Figma.fonthelper
+
+# Java
+winget install -e --id Oracle.JavaRuntimeEnvironment
+winget install -e --id Oracle.JDK.19
+
+# Other
+winget install -e --id Canonical.Ubuntu.2204
+
+# Unsure whether to install or not
+winget install -e --id undergroundwires.privacy.sexy
+winget install -e --id Tyrrrz.LightBulbW
